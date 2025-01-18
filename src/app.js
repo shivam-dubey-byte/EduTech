32cd6999e83bccf1ed8270be0efbcf049e8726e3
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2025-01-19 04:59:46 +0530

    0.0.2

--- src/app.js
+++ src/app.js
@@ -12,8 +12,10 @@ app.use(cors());
 app.use('/auth', authRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI_PRO, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = app;
+//srd191104
+//ECWKUWWYT2QHPWR1
