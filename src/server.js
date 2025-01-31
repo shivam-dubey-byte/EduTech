196666782c88aae77fb2453a5393d86748049979
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2025-01-31 19:58:41 +0530

    0.0.7 update working

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
-const PORT = 5001;//process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 
 
-app.listen(PORT, () => {
+app.listen(3001, () => {
   console.log(`Auth Service running on http://localhost:${PORT}`);
 });
